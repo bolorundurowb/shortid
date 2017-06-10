a9db9c5b58155398dc0095de5edfef33aec0a0a9
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-06-10 23:08:01 +0100

    feature: adds special character support

--- shortid/ShortId.cs
+++ shortid/ShortId.cs
@@ -8,30 +8,37 @@ namespace shortid
         private const string capitals = "ABCDEFGHIJKLMNOPQRSTUVWXY";
         private const string smalls = "abcdefghjlkmnopqrstuvwxyz";
         private const string numbers = "0123456789";
+        private const string specials = "-_";
         
         /// <summary>
         /// Generates a random string of varying length
         /// </summary>
         /// <param name="useNumbers">Whether or not to include numbers</param>
+        /// <param name="useSpecial">Whether or not special characters are included</param>
         /// <returns>A random string</returns>
-        public static string Generate(bool useNumbers = false)
+        public static string Generate(bool useNumbers = false, bool useSpecial = true)
         {
             int length = _random.Next(7, 15);
-            return Generate(useNumbers, length);
+            return Generate(useNumbers, useSpecial, length);
         }
 
         /// <summary>
         /// Generates a random string of a specified length
         /// </summary>
         /// <param name="useNumbers">Whether or not numbers are included in the string</param>
+        /// <param name="useSpecial">Whether or not special characters are included</param>
         /// <param name="length">The length of the generated string</param>
         /// <returns>A random string</returns>
-        public static string Generate(bool useNumbers, int length)
+        public static string Generate(bool useNumbers, bool useSpecial, int length)
         {
             string pool = $"{capitals}{smalls}";
             if (useNumbers)
             {
                 pool += numbers;
+            }
+            if (useSpecial)
+            {
+                pool += specials;
             }
             
             string output = string.Empty;
