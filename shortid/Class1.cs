75823693f8ce89298077ea1c98406fdf7f5117af
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-06-10 14:16:11 +0100

    chore: renames code file and adds basics

--- shortid/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace shortid
-{
-    public class Class1
-    {
-    }
-}
---
+++ shortid/ShortId.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace shortid
+{
+    public class ShortId
+    {
+        private static Random _random = new Random();
+        
+        public static string Generate()
+        {
+            return String.Empty;
+        }
+    }
+}
