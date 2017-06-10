9ffaf9e580d7bf4038b4432eca51afdbd0c16e36
Bolorunduro Winner-Timothy B <ogatimo@gmail.com>
2017-06-10 22:23:36 +0100

    feature: adds two generator functions

--- shortid/ShortId.cs
+++ shortid/ShortId.cs
@@ -5,10 +5,31 @@ namespace shortid
     public class ShortId
     {
         private static Random _random = new Random();
+        private const string capitals = "ABCDEFGHIJKLMNOPQRSTUVWXY";
+        private const string smalls = "abcdefghjlkmnopqrstuvwxyz";
+        private const string numbers = "0123456789";
         
-        public static string Generate()
+        public static string Generate(bool useNumbers = false)
         {
-            return String.Empty;
+            int length = _random.Next(7, 15);
+            return Generate(useNumbers, length);
         }
+
+        public static string Generate(bool useNumbers, int length)
+        {
+            string pool = $"{capitals}{smalls}";
+            if (useNumbers)
+            {
+                pool += numbers;
+            }
+            
+            string output = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                int charIndex = _random.Next(0, pool.Length);
+                output += pool[charIndex];
+            }
+            return output;
+         }
     }
 }
