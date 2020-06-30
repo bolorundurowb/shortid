75d0260ee0e5f3fa16e4fced99d6ed833c5d7191
Jonathan <vanillajonathan@users.noreply.github.com>
2020-06-30 11:00:56 +0200

    End sentences with a period

--- shortid/Configuration/GenerationOptions.cs
+++ shortid/Configuration/GenerationOptions.cs
@@ -5,20 +5,20 @@ namespace shortid.Configuration
     public class GenerationOptions
     {
         /// <summary>
-        /// Determines whether numbers are used in generating the id
-        /// Default: false
+        /// Determines whether numbers are used in generating the id.
+        /// Default: false.
         /// </summary>
         public bool UseNumbers { get; set; }
 
         /// <summary>
-        /// Determines whether special characters are used in generating the id
-        /// Default: true
+        /// Determines whether special characters are used in generating the id.
+        /// Default: true.
         /// </summary>
         public bool UseSpecialCharacters { get; set; } = true;
 
         /// <summary>
-        /// Determines the length of the generated id
-        /// Default: a random length between 7 and 15
+        /// Determines the length of the generated id.
+        /// Default: a random length between 7 and 15.
         /// </summary>
         public int Length { get; set; } =
             RandomUtils.GenerateNumberInRange(Constants.MinimumAutoLength, Constants.MaximumAutoLength);
