f0eda9f8f6b16c4d118935379344e9c07eefb3fb
Winner-Timothy Bolorunduro <ogatimo@gmail.com>
2020-08-25 12:10:39 +0100

    ft: update the constants

--- shortid/Utils/Constants.cs
+++ shortid/Utils/Constants.cs
@@ -2,8 +2,8 @@ namespace shortid.Utils
 {
     internal static class Constants
     {
-        public const int MinimumAutoLength = 7;
+        public const int MinimumAutoLength = 8;
 
-        public const int MaximumAutoLength = 15;
+        public const int MaximumAutoLength = 20;
     }
 }
