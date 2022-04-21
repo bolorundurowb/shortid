1ee7d93c3e25f5428c05a1a7d5c73db4f839a37e
Bolorunduro Winner-Timothy <ogatimo@gmail.com>
2022-04-21 20:31:26 +0100

    ch: remove unnecessary braces

--- src/shortid/ShortId.cs
+++ src/shortid/ShortId.cs
@@ -20,60 +20,6 @@ namespace shortid
         private static readonly object ThreadLock = new();
 
         /// <summary>
-        /// Generates a random string of varying length with special characters and without numbers.
-        /// </summary>
-        /// <param name="useNumbers">Whether or not to include numbers.</param>
-        /// <param name="useSpecial">Whether or not special characters are included.</param>
-        /// <returns>A random string.</returns>
-        [Obsolete("Use the Generate(options) overload instead")]
-        public static string Generate(bool useNumbers = false, bool useSpecial = true)
-        {
-            var options = new GenerationOptions
-            {
-                UseNumbers = useNumbers,
-                UseSpecialCharacters = useSpecial
-            };
-
-            return Generate(options);
-        }
-
-        /// <summary>
-        /// Generates a random string of a specified length with the option to add numbers and special characters.
-        /// </summary>
-        /// <param name="useNumbers">Whether or not numbers are included in the string.</param>
-        /// <param name="useSpecial">Whether or not special characters are included.</param>
-        /// <param name="length">The length of the generated string.</param>
-        /// <returns>A random string.</returns>
-        [Obsolete("Use the Generate(options) overload instead")]
-        public static string Generate(bool useNumbers, bool useSpecial, int length)
-        {
-            var options = new GenerationOptions
-            {
-                UseNumbers = useNumbers,
-                UseSpecialCharacters = useSpecial,
-                Length = length
-            };
-
-            return Generate(options);
-        }
-
-        /// <summary>
-        /// Generates a random string of a specified length with special characters and without numbers.
-        /// </summary>
-        /// <param name="length">The length of the generated string.</param>
-        /// <returns>A random string.</returns>
-        [Obsolete("Use the Generate(options) overload instead")]
-        public static string Generate(int length)
-        {
-            var options = new GenerationOptions
-            {
-                Length = length
-            };
-
-            return Generate(options);
-        }
-
-        /// <summary>
         /// Generates a random string to match the specified options.
         /// </summary>
         /// <param name="options">The generation options.</param>
@@ -82,40 +28,27 @@ namespace shortid
         /// <exception cref="ArgumentException">Thrown when options.Length is less than 8.</exception>
         public static string Generate(GenerationOptions options)
         {
-            if (options == null)
-            {
-                throw new ArgumentNullException(nameof(options));
-            }
+            if (options == null) throw new ArgumentNullException(nameof(options));
 
             if (options.Length < Constants.MinimumAutoLength)
-            {
                 throw new ArgumentException(
                     $"The specified length of {options.Length} is less than the lower limit of {Constants.MinimumAutoLength} to avoid conflicts.");
-            }
 
             var characterPool = _pool;
             var poolBuilder = new StringBuilder(characterPool);
-            if (options.UseNumbers)
-            {
-                poolBuilder.Append(Numbers);
-            }
+            if (options.UseNumbers) poolBuilder.Append(Numbers);
 
-            if (options.UseSpecialCharacters)
-            {
-                poolBuilder.Append(Specials);
-            }
+            if (options.UseSpecialCharacters) poolBuilder.Append(Specials);
 
             var pool = poolBuilder.ToString();
 
             var output = new char[options.Length];
             for (var i = 0; i < options.Length; i++)
-            {
                 lock (ThreadLock)
                 {
                     var charIndex = _random.Next(0, pool.Length);
                     output[i] = pool[charIndex];
                 }
-            }
 
             return new string(output);
         }
@@ -128,10 +61,7 @@ namespace shortid
         /// <exception cref="InvalidOperationException">Thrown when the new character set is less than 50 characters.</exception>
         public static void SetCharacters(string characters)
         {
-            if (string.IsNullOrWhiteSpace(characters))
-            {
-                throw new ArgumentException("The replacement characters must not be null or empty.");
-            }
+            if (string.IsNullOrWhiteSpace(characters)) throw new ArgumentException("The replacement characters must not be null or empty.");
 
             var charSet = characters
                 .ToCharArray()
@@ -140,10 +70,8 @@ namespace shortid
                 .ToArray();
 
             if (charSet.Length < Constants.MinimumCharacterSetLength)
-            {
                 throw new InvalidOperationException(
                     $"The replacement characters must be at least {Constants.MinimumCharacterSetLength} letters in length and without whitespace.");
-            }
 
             lock (ThreadLock)
             {
