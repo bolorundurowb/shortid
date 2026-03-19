7815cfb11b09d17ceacce0cd389a7c6a085c80c5
Winner-Timothy Bolorunduro <bolorundurowb@gmail.com>
2026-03-19 10:11:51 +0100

    ft: add benchmarking project to evaluate ID generation performance under various configurations

---
+++ src/shortid.Benchmarks/ShortIdLengthBenchmarks.cs
@@ -0,0 +1,26 @@
+using BenchmarkDotNet.Attributes;
+using shortid;
+
+namespace shortid.Benchmarks;
+
+/// <summary>
+/// Throughput vs length (default-style pool: letters + specials, no numbers).
+/// </summary>
+[MemoryDiagnoser]
+public class ShortIdLengthBenchmarks
+{
+    [Params(8, 9, 10, 11, 12, 13, 14, 15)]
+    public int Length { get; set; }
+
+    private ShortIdOptions _options = null!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        ShortId.Reset();
+        _options = new ShortIdOptions(useNumbers: false, useSpecialCharacters: true, length: Length);
+    }
+
+    [Benchmark]
+    public string Generate() => ShortId.Generate(_options);
+}
